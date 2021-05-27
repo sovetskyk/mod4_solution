05a3ebe04ecba237f0c900d4388a9f247cb5f5bc
Ekaterina Savostina <74173325+sovetskyk@users.noreply.github.com>
2021-05-27 15:44:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// helloSpeaker function
+const helloSpeaker = {
+  speakWord: "Hello",
+};
+
+(function () {
+  helloSpeaker.speak = function (name) {
+    return helloSpeaker.speakWord + " " + name;
+  };
+})();
+
+//byeSpeaker function
+const byeSpeaker = {
+  speakWord: "Good Bye",
+};
+
+(function () {
+  byeSpeaker.speak = function (name) {
+    return byeSpeaker.speakWord + " " + name;
+  };
+})();
+
+// main function
+(function () {
+  var names = [
+    "Yaakov",
+    "John",
+    "Jen",
+    "Jason",
+    "Paul",
+    "Frank",
+    "Larry",
+    "Paula",
+    "Laura",
+    "Jim",
+  ];
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    let name = names[i];
+    if (firstLetter === "j") {
+      document.write(byeSpeaker.speak(name) + "<br>");
+    } else {
+      document.write(helloSpeaker.speak(name) + "<br>");
+    }
+  }
+})();
